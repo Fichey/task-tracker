9544cc5d7d51b4d6727f621b4f48abdce74bbaa4
Fichey <czarekpolk@gmail.com>
2022-06-12 14:11:54 +0200

    update

---
+++ src/components/AddTask.js
@@ -0,0 +1,14 @@
+
+
+const AddTask = () => {
+  return (
+    <form className="add-form">
+        <div className="form-control">
+            <label>Tasl</label>
+            <input type="text" placeholder="Add Task"/>
+        </div>
+    </form>
+  )
+}
+
+export default AddTask
